c845ee035d9544a49c40f70a27b45a28406a871a
KyleAxley <kanderson7645@yahoo.com>
2022-10-10 20:24:49 -0400

    added jwt auth and connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
+
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+//import for middleware function
+const { authMiddleware } = require('./utils/auth');
+
+//import to typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.listen(PORT, () => {
-    console.log(`API running on port ${PORT}`);
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API running on port ${PORT}`);
+    })
 })
 
 
