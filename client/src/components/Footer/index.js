13cc54680f0bb0c86e6e48349a0a6343f3d97b31
KyleAxley <kanderson7645@yahoo.com>
2022-10-13 23:47:44 -0400

    started footer and header components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                &copy; 2022 King Daddy Donkey Dong
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
