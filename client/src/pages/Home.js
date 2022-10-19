c67f46a64882f38a05842c3e616b0856ba368a13
KyleAxley <kanderson7645@yahoo.com>
2022-10-18 21:22:04 -0400

    added how to use card for logged in user

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import ChoresList from "../components/ChoresList";
-import RewardsList from "../components/RewardsList";
 import Auth from "../utils/auth";
 import Card from "react-bootstrap/Card";
 import homepic from "../assets/images/CleanModernBathroom.jpg";
@@ -9,8 +7,38 @@ const Home = () => {
   return (
     <main>
       {Auth.loggedIn() ? (
-        <div>
-          <h1> This will be a suggestion board</h1>
+        <div className="card-style">
+          <Card
+            style={{
+              width: "50rem",
+              backgroundColor: "var(--yellow)",
+              inlineSize: "1000px",
+            }}
+          >
+            <Card.Header>
+              <h1 className="home-page-title">How to use this application</h1>
+              <ul className="home-page">
+                <li>
+                    Navigate to your profile page
+                </li>
+                <li>
+                    Add your children's names to your profile
+                </li>
+                <li>
+                    Add a chore including a brief description and point value
+                </li>
+                <li>
+                    Add a reward including a brief description and cost value
+                </li>
+                <li>
+                    Once the chore is completed, check the "completed by" button to assign points to that child.
+                </li>
+                <li>
+                    Each child will accumalte points after completing chores to spend on a reward of choice. 
+                </li>
+              </ul>
+            </Card.Header>
+          </Card>
         </div>
       ) : (
         <div className="card-style">
@@ -22,21 +50,15 @@ const Home = () => {
             }}
           >
             <Card.Header>
-              <h1 className="header-title">Welcome to ChoreBoard</h1>
+              <h1 className="home-page-title">Welcome to ChoreBoard!</h1>
               <img
                 src={homepic}
                 className="home-pic"
                 alt="clean bathroom"
               ></img>
 
-              <p className="header-title">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <p className="home-page">
+                Welcome to the Chore Board application where we make it fun and rewarding for both parents and children. As the user you will assign household chores to be completed and create rewards for your children. Your children will then be able to purchase the rewards with the points they have accumulated from doing chores!  
               </p>
             </Card.Header>
           </Card>
