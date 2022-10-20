28c63cc6a6ced4bead3d8897447b68b1ddf33c2b
KyleAxley <kanderson7645@yahoo.com>
2022-10-19 23:05:23 -0400

    added rewards table, add reward modal

--- client/src/components/ChoresList/index.js
+++ client/src/components/ChoresList/index.js
@@ -1,33 +1,29 @@
 import React, { useState } from "react";
 // import { useMutation, useQuery } from '@apollo/client';
 // import { ADD_CHORE, EDIT_CHORE, REMOVE_CHORE } from '../../utils/mutations';
-import { Modal, Button, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Modal, Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 const ChoresList = () => {
-    const [show, setShow] = useState(false);
-    const [tabSelected, setTabSelected] = useState(1);
-    const toggleTab = (index) => {
-      setTabSelected(index);
-    };
-    
-    const handleClose = () => setShow(false);
+  const [show, setShow] = useState(false);
+  const [tabSelected, setTabSelected] = useState(1);
+  const toggleTab = (index) => {
+    setTabSelected(index);
+  };
 
+  const handleClose = () => setShow(false);
 
-    const handleShow = () => {
-    
-      tabSelected === 1 && setShow(true)
-    }
-    
-    // const { loading, error, data } = useQuery(QUERY_MYCHORES);
-    // if (loading) return 'Loading...';
-    // if (error) return `Error! ${error.message}`;
+  const handleShow = () => {
+    tabSelected === 1 && setShow(true);
+  };
 
-        
+  // const { loading, error, data } = useQuery(QUERY_MYCHORES);
+  // if (loading) return 'Loading...';
+  // if (error) return `Error! ${error.message}`;
 
-    return (
+  return (
     <>
-        {/* <div className="container">
+      {/* <div className="container">
 
             <div className=" choreBoard">
                 <div className="choreBoardHeader">
@@ -40,91 +36,119 @@ const ChoresList = () => {
             </div>
 
         </div> */}
-        <div className="container container--chores p-0">
-      <div className="cb-tabs--chores">
-        <button
-          className={tabSelected === 1 ? "tabs--chores active-tabs--chores tab-1--chores noBorder--chores" : "tabs--chores noBorder--chores"}
-          onClick={() => toggleTab(1)}
-        >
-          To-Do
-        </button>
-        <button
-          className={tabSelected === 2 ? "tabs--chores active-tabs--chores tab-2--chores noBorder--chores" : "tabs--chores noBorder--chores"}
-          onClick={() => toggleTab(2)}
-        >
-          Completed
-        </button>
-        <Button variant="primary" className={tabSelected === 1 ? "rightNav--chores addChoreBtn--chores":" rightNav--chores addChoreBtn--chores disableBtn--chores"} onClick={handleShow}>
-                        + Add Chore
-                    </Button>
-      </div>
-        
-      <div className="content-tabs--chores">
-        <div
-          className={tabSelected === 1 ? "content--chores  active-content--chores" : "content--chores"}
-        >
-            <div className="boardHeader--chores">
-          <h2>My Chore Board</h2>
-          
-            </div>
-          
+      <div className="container container--chores p-0">
+        <div className="cb-tabs--chores">
+          <button
+            className={
+              tabSelected === 1
+                ? "tabs--chores active-tabs--chores tab-1--chores noBorder--chores"
+                : "tabs--chores noBorder--chores"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            To-Do
+          </button>
+          <button
+            className={
+              tabSelected === 2
+                ? "tabs--chores active-tabs--chores tab-2--chores noBorder--chores"
+                : "tabs--chores noBorder--chores"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            Completed
+          </button>
+          <Button
+            variant="primary"
+            className={
+              tabSelected === 1
+                ? "rightNav--chores addChoreBtn--chores"
+                : " rightNav--chores addChoreBtn--chores disableBtn--chores"
+            }
+            onClick={handleShow}
+          >
+            + Add Chore
+          </Button>
         </div>
 
-        <div
-          className={tabSelected === 2 ? "content--chores  active-content--chores" : "content--chores"}
-        >
+        <div className="content-tabs--chores">
+          <div
+            className={
+              tabSelected === 1
+                ? "content--chores  active-content--chores"
+                : "content--chores"
+            }
+          >
             <div className="boardHeader--chores">
-                
-          <h2>Completed Chores</h2>
+              <h2>My Chore Board</h2>
             </div>
+          </div>
+
+          <div
+            className={
+              tabSelected === 2
+                ? "content--chores  active-content--chores"
+                : "content--chores"
+            }
+          >
+            <div className="boardHeader--chores">
+              <h2>Completed Chores</h2>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-            <Modal show={show} onHide={handleClose} className="addChoreModal--chores">
-                <Modal.Header closeButton>
-                    <Modal.Title>New Chore</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="ChoreForm.ControlInput1">
-                            <Form.Label>Chore Name:</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="choreName"
-                                placeholder="Enter Chore Here"
-                                required
-                                autoFocus
-                                />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="ChoreForm.ControlTextarea1"
-                            >
-                            <Form.Label>Chore Description:</Form.Label>
-                            <Form.Control as="textarea" name="choreDesc" rows={3} placeholder="Briefly Describe The Specifics Of The Chore" />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="ChoreForm.ControlTextarea2"
-                            >
-                            <Form.Label>Point Value:</Form.Label>
-                            <Form.Control className="text-secondary" name="chorePoints" style={{ width: 75 }} type="number" step="5" min="5" placeholder="5" required max="50"></Form.Control>
-
-                        </Form.Group>
-                    </Form>
-                    
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="danger" onClick={handleClose}>
-                        Cancel
-                    </Button>
-                    <Button type="submit" variant="success" onClick={handleClose}>
-                        Add Chore
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+      <Modal show={show} onHide={handleClose} className="addChoreModal--chores">
+        <Modal.Header closeButton>
+          <Modal.Title>New Chore</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="ChoreForm.ControlInput1">
+              <Form.Label>Chore Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="choreName"
+                placeholder="Enter Chore Here"
+                required
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="ChoreForm.ControlTextarea1">
+              <Form.Label>Chore Description:</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="choreDesc"
+                rows={3}
+                placeholder="Briefly Describe The Specifics Of The Chore"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="ChoreForm.ControlTextarea2">
+              <Form.Label>Point Value:</Form.Label>
+              <Form.Control
+                className="text-secondary"
+                name="chorePoints"
+                style={{ width: 75 }}
+                type="number"
+                step="5"
+                min="5"
+                placeholder="5"
+                required
+                max="50"
+              ></Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button type="submit" variant="success" onClick={handleClose}>
+            Add Chore
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-    );
-}
+  );
+};
 
 export default ChoresList;
--- client/src/components/RewardsList/index.js
+++ client/src/components/RewardsList/index.js
@@ -1,9 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Modal, Button } from "react-bootstrap";
 
 const RewardsList = () => {
+    const [show, setShow] = useState(false);
+    const [tabSelected, setTabSelected] = useState(1);
+    const toggleTab = (index) => {
+      setTabSelected(index);
+    };
+  
+    const handleClose = () => setShow(false);
+  
+    const handleShow = () => {
+      tabSelected === 1 && setShow(true);
+    };
+  
+  
     return (
-        <h2>I AM A REWARDSLIST</h2>
-    );
+      <>
+        <div className="container container--rewards p-0">
+          <div className="cb-tabs--rewards">
+            <button
+              className={
+                tabSelected === 1
+                  ? "tabs--rewards active-tabs--rewards tab-1--rewards noBorder--rewards"
+                  : "tabs--rewards noBorder--rewards"
+              }
+              onClick={() => toggleTab(1)}
+            >
+              Rewards
+            </button>
+            <button
+              className={
+                tabSelected === 2
+                  ? "tabs--rewards active-tabs--chores tab-2--chores noBorder--rewards"
+                  : "tabs--rewards noBorder--rewards"
+              }
+              onClick={() => toggleTab(2)}
+            >
+              Claimed
+            </button>
+            <Button
+              variant="primary"
+              className={
+                tabSelected === 1
+                  ? "rightNav--rewards addChoreBtn--rewards"
+                  : " rightNav--rewards addChoreBtn--rewards disableBtn--rewards"
+              }
+              onClick={handleShow}
+            >
+              + Add Reward
+            </Button>
+          </div>
+  
+          <div className="content-tabs--rewards">
+            <div
+              className={
+                tabSelected === 1
+                  ? "content--rewards  active-content--rewards"
+                  : "content--rewards"
+              }
+            >
+              <div className="boardHeader--rewards">
+                <h2>Rewards</h2>
+              </div>
+            </div>
+  
+            <div
+              className={
+                tabSelected === 2
+                  ? "content--rewards  active-content--rewards"
+                  : "content--rewards"
+              }
+            >
+              <div className="boardHeader--rewards">
+                <h2>Claimed</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Modal show={show} onHide={handleClose} className="addChoreModal--rewards">
+          <Modal.Header closeButton>
+            <Modal.Title>New Reward</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="ChoreForm.ControlInput1">
+                <Form.Label>Reward Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="rewardName"
+                  placeholder="Enter Reward Here"
+                  required
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="ChoreForm.ControlTextarea1">
+                <Form.Label>Reward Description:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="rewardDesc"
+                  rows={3}
+                  placeholder="Briefly Describe The Specifics Of The Reward"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="ChoreForm.ControlTextarea2">
+                <Form.Label>Cost:</Form.Label>
+                <Form.Control
+                  className="text-secondary"
+                  name="rewardPoints"
+                  style={{ width: 75 }}
+                  type="number"
+                  step="5"
+                  min="5"
+                  placeholder="5"
+                  required
+                  max="50"
+                ></Form.Control>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" variant="success" onClick={handleClose}>
+              Add Reward
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+  );
 };
 
 export default RewardsList;
