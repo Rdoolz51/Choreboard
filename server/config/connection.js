97af73ef8162c837de101cc5626b7b23cc6be8f1
Rdoolz51 <Rdoolz51@aol.com>
2022-10-20 17:48:49 -0400

    connected DB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Choreboard', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Choreboard', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     // useCreateIndex: true,
+//     // useFindAndModify: false
+// });
+
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.agjhbbl.mongodb.net/?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false
+}, (err) => {
+    if (err) throw err;
+
+    console.log("DB CONNECTED SUCCESSFULLY!");
 });
 
 module.exports = mongoose.connection;
